735dc80f2c02fda148c9bc2a3f26e9d3aed7f59e
Manolis Kalafatis <mankal111@hotmail.com>
2018-10-04 17:38:47 +0300

    Created getFloat function

--- index.js
+++ index.js
@@ -9,8 +9,18 @@
 const getInteger = (min, max) => 
     Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 
+/**
+ * Return a random float
+ * @param {number} minimum
+ * @param {number} maximum
+ * @return {number}
+ */
+const getFloat = (min, max) =>
+    Math.random() * (max - min) + min;
+
 const JXRand = {
-    getInteger: (min, max) => getInteger(min, max)
+    getInteger,
+    getFloat
 }
 
 export default { JXRand };
