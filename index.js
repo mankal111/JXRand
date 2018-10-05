4f7d7731e53b816c174639a946779e68c3834693
Manolis Kalafatis <mankal111@hotmail.com>
2018-10-05 19:03:05 +0300

    created getInterval function

--- index.js
+++ index.js
@@ -28,9 +28,49 @@ const getFloat = (min, max) => {
     }
 }
 
+/**
+ * Returns a random interval
+ * @param {object} options - The limits of the random interval
+ * @param {number} options.min - Minimum possible endpoint
+ * @param {number} options.max - Maximum possible endpoint
+ * @param {number} [options.minLength] - Minimum possible length
+ * @param {number} [options.maxLength] - Maximum possible length
+ * @param {string} [options.type=float] - The type of the endpoints
+ * @returns {number[]} Array with the two endpoints of the random interval
+ */
+const getInterval = (options) => {
+    if (typeof options !== 'object') {
+        throw "Parameter is not an object";
+    } else if (isNaN(options.min) || isNaN(options.max)) {
+        throw "Parameter property 'min' or 'max' is not a number";
+    } else if (options.min > options.max) {
+        throw "Parameter property 'min' should be less or equal than 'max'";
+    } else {
+        const minLength = options.minLength || 0;
+        const maxLength = options.maxLength || options.max - options.min;
+        const type = options.type || 'float';
+        let getRandomNumberFunction;
+        switch (type) {
+            case 'float':
+                getRandomNumberFunction = getFloat;
+                break;
+            case 'integer':
+                getRandomNumberFunction = getInteger;
+                break;
+            default:
+                throw "Not supported type";
+        }
+        const intervalLength = getRandomNumberFunction(minLength, maxLength);
+        const leftEndpoint = getRandomNumberFunction(options.min, options.max - intervalLength);
+        const rightEndpoint = leftEndpoint + intervalLength;
+        return [leftEndpoint, rightEndpoint];
+    }
+}
+
 const JXRand = {
     getInteger,
-    getFloat
+    getFloat,
+    getInterval
 }
 
 module.exports = JXRand;
